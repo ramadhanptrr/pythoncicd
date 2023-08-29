b6deababaea3b21b3037e66df657aa8605d87401
rama <ramadhanputraaf@gmail.com>
2023-08-30 01:13:13 +0700

    no message

--- module/validation.py
+++ module/validation.py
@@ -2,7 +2,7 @@ import os
 import sys
 business_key = os.getenv("business_key")
 print("validation starts")
-if (len(business_key) != 0) : 
+if (business_key is None) : 
     print('passed')
 else :
     print("Required")
