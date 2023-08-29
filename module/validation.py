b72f59dc824a2357957f8f0f36ae694465cd7f35
rama <ramadhanputraaf@gmail.com>
2023-08-30 01:18:53 +0700

    no message

--- module/validation.py
+++ module/validation.py
@@ -3,7 +3,7 @@ import sys
 business_key = os.getenv("business_key")
 print("validation starts")
 if (business_key is None) : 
-    sys.exit('Required')
+    sys.exit('Column is Required')
 else :
     print('passed')
     
