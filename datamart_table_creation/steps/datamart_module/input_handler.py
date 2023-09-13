c41420771dc3ee565119e8f5060e13af18759165
rama <ramadhanputraaf@gmail.com>
2023-09-13 22:41:14 +0700

    no message

--- datamart_table_creation/steps/datamart_module/input_handler.py
+++ datamart_table_creation/steps/datamart_module/input_handler.py
@@ -9,7 +9,11 @@ class InputHandler:
         sanitize_results = {}
         input_dict = {
             "table_name": os.getenv('table_name'),
-            "table_type": os.getenv('table_type')
+            "table_type": os.getenv('table_type'),
+            "business_key": os.getenv('business_key'),
+            "incremental_key": os.getenv('incremental_key'),
+            "parent_dag": os.getenv('parent_dag'),
+            "redshift_ddl": os.getenv('redshift_ddl')
         }
         for key, value in input_dict.items():
             if isinstance(value, str):
--- datamart_table_creation/steps/validation_steps.py
+++ datamart_table_creation/steps/validation_steps.py
@@ -8,7 +8,11 @@ input_value = InputHandler.InputValue()
 
 rules = [
     Validator('table_name',input_value["table_name"]).set_required().set_min_length(3).set_max_length(127),
-    Validator('table_type',input_value["table_type"]).set_required().set_dropdown_list(["presentations"])
+    Validator('table_type',input_value["table_type"]).set_required().set_dropdown_list(["presentations"]),
+    Validator('business_key',input_value["business_key"]).set_required().set_min_length(2).set_max_length(127),
+    Validator('incremental_key',input_value["incremental_key"]).set_required().set_valid_list(),
+    Validator('parent_dag',input_value["parent_dag"]).set_required(),
+    Validator('redshift_ddl',input_value["redshift_ddl"]).set_required()
 ]
 
 validation_results = ValidationRule(rules)
