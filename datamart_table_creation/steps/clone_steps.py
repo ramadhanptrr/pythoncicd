045f8503dd48c34533c49fa941fa4daeafb46654
rama <ramadhanputraaf@gmail.com>
2023-10-31 21:48:48 +0700

    no message

--- datamart_table_creation/steps/clone_steps.py
+++ datamart_table_creation/steps/clone_steps.py
@@ -7,15 +7,15 @@ from datamart_module.input_handler import InputHandler
 
 #source and target folder
 config = YAMLConfigLoader('./config.yaml')
-input_value = InputHandler.InputValue()
+# input_value = InputHandler.InputValue()
+# table_name = input_value['table_name']
+# new_filename = f"load_process_to_pl_{table_name}_dag.py"
 config.load_config()
-repository_folder = config.key('path_cfg')["repo_dir"]
-destination_directory = config.key('path_cfg')["repo_pl_dir"]
-master_filename = config.key('path_cfg')["pl_master_filename"]
-master_directory = config.key('path_cfg')["dag_master_directory"]
+# repository_folder = config.key('path_cfg')["repo_dir"]
+# destination_directory = config.key('path_cfg')["repo_pl_dir"]
+# master_filename = config.key('path_cfg')["pl_master_filename"]
+# master_directory = config.key('path_cfg')["dag_master_directory"]
 branch = config.key('airflow_dag_repo')["branch"]
-table_name = input_value['table_name']
-new_filename = f"load_process_to_pl_{table_name}_dag.py"
 
 #check & drop if folder exists
 if FileOperations(repository_folder).is_directory_exists() :
@@ -25,5 +25,5 @@ if FileOperations(repository_folder).is_directory_exists() :
 FileOperations(repository_folder).create_directory()
 
 #cloning the git
-# GitManager('airflow_dag_repo').clone()
-# GitManager('airflow_dag_repo').pull(branch)
+GitManager('airflow_dag_repo').clone()
+GitManager('airflow_dag_repo').pull(branch)
