daaf8d9f741317be7a5149675e92b703a2427c1b
rama <ramadhanputraaf@gmail.com>
2023-10-31 22:39:55 +0700

    no message

--- datamart_table_creation/steps/push_airflow_dag_steps.py
+++ datamart_table_creation/steps/push_airflow_dag_steps.py
@@ -12,6 +12,7 @@ repository_folder = config.key('path_cfg')["repo_dir"]
 destination_directory = config.key('path_cfg')["repo_pl_dir"]
 master_directory = config.key('path_cfg')["dag_master_directory"]
 master_filename = config.key('path_cfg')["pl_master_filename"]
+branch = config.key('airflow_dag_repo')["branch"]
 table_name = table_name = input_value['table_name']
 new_filename = f'load_process_to_pl_{table_name}_dag.py'
 
@@ -19,6 +20,5 @@ if not FileOperations(destination_directory).is_file_exists(new_filename):
     FileOperations(destination_directory).copy(f"{master_directory}/{master_filename}")
     FileOperations(destination_directory).rename(master_filename,new_filename)
     GitManager('airflow_dag_repo').push(f"presentations/{new_filename}", branch)
-
 else:
     sys.exit('file already exists')
